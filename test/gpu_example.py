8ff657f3b3e05979baeb9192593b7bd560d4dfa9
Alex Kim <40706779+0b10010010@users.noreply.github.com>
2018-11-14 16:25:51 -0600

    Update gpu_example.py

--- test/gpu_example.py
+++ test/gpu_example.py
@@ -1,43 +1,67 @@
+#! /usr/bin/env python
+
+'''
+Example vectorize usage.
+'''
+from __future__ import print_function
+
+import math
+import sys
+from timeit import default_timer as time
+
 import numpy as np
-from timeit import default_timer as timer
+
+
 from numba import vectorize
- 
-# This should be a substantially high value. On my test machine, this took
-# 33 seconds to run via the CPU and just over 3 seconds on the GPU.
-NUM_ELEMENTS = 100000000
- 
-# This is the CPU version.
-def vector_add_cpu(a, b):
-  c = np.zeros(NUM_ELEMENTS, dtype=np.float32)
-  for i in range(NUM_ELEMENTS):
-    c[i] = a[i] + b[i]
-  return c
- 
-# This is the GPU version. Note the @vectorize decorator. This tells
-# numba to turn this into a GPU vectorized function.
-@vectorize(["float32(float32, float32)"], target='cuda')
-def vector_add_gpu(a, b):
-  return a + b;
- 
+
+
+N = 1000000
+
+
+def check_answer(ans, A, B, C):
+    for d, a, b in zip(ans, A, B):
+        gold = sum(a, b)
+        assert np.allclose(d, gold), (d, gold)
+
+def sum(a, b):
+    return a + b
+
 def main():
-  a_source = np.ones(NUM_ELEMENTS, dtype=np.float32)
-  b_source = np.ones(NUM_ELEMENTS, dtype=np.float32)
- 
-  # Time the CPU function
-  start = timer()
-  vector_add_cpu(a_source, b_source)
-  vector_add_cpu_time = timer() - start
- 
-  # Time the GPU function
-  start = timer()
-  vector_add_gpu(a_source, b_source)
-  vector_add_gpu_time = timer() - start
- 
-  # Report times
-  print("CPU function took %f seconds." % vector_add_cpu_time)
-  print("GPU function took %f seconds." % vector_add_gpu_time)
- 
-  return 0
- 
-if __name__ == "__main__":
-  main()
+    targets = ['cpu', 'parallel']
+
+    # run just one target if is specified in the argument
+    for t in targets:
+        if t in sys.argv[1:]:
+            targets = [t]
+            break
+
+    for target in targets:
+        print('== Target', target)
+        vect_sum = vectorize(['f4(f4, f4)', 'f8(f8, f8)'],
+                             target=target)(sum)
+
+        A = np.random.random(N).astype(np.float32)
+        B = np.random.random(N).astype(np.float32)
+        assert A.shape == B.shape
+        assert A.dtype ==  B.dtype
+        assert len(A.shape) == 1
+
+        D = np.empty(A.shape, dtype=A.dtype)
+
+        print('Data size', N)
+
+        ts = time()
+        D = vect_sum(A, B)
+        te = time()
+
+        total_time = (te - ts)
+
+        print('Execution time %.4f' % total_time)
+        print('Throughput %.4f' % (N / total_time))
+
+        if '-verify' in sys.argv[1:]:
+            check_answer(D, A, B, C)
+
+
+if __name__ == '__main__':
+    main()
