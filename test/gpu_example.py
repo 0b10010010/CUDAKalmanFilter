2638a29a2cd9a6dae4d647964f36f39209eb8d0f
Alex Kim <alexkim92@ksu.edu>
2018-11-14 19:46:27 -0600

    Modified test file

--- test/gpu_example.py
+++ test/gpu_example.py
@@ -1,67 +1,34 @@
 #! /usr/bin/env python
 
-'''
-Example vectorize usage.
-'''
-from __future__ import print_function
-
-import math
-import sys
-from timeit import default_timer as time
-
 import numpy as np
-
-
+from timeit import default_timer as timer
 from numba import vectorize
 
-
-N = 100000000
-
-
-def check_answer(ans, A, B, C):
-    for d, a, b in zip(ans, A, B):
-        gold = sum(a, b)
-        assert np.allclose(d, gold), (d, gold)
-
-def sum(a, b):
+@vectorize(["float32(float32, float32)"], target='cuda')
+def VectorAdd_GPU(a, b):
     return a + b
 
+def VectorAdd_CPU(a, b, c):
+    for i in range(a.size):
+        c[i] = a[i] + b[i]
+
 def main():
-    targets = ['cpu', 'parallel']
+    N = 3200000
+    
+    A = np.ones(N, dtype=np.float32)
+    B = np.ones(N, dtype=np.float32)
+    C = np.zeros(N, dtype=np.float32)
+    
+    start = timer()
+    VectorAdd_CPU(A, B, C)
+    vectoradd_cpu_time = timer() - start
 
-    # run just one target if is specified in the argument
-    for t in targets:
-        if t in sys.argv[1:]:
-            targets = [t]
-            break
-
-    for target in targets:
-        print('== Target', target)
-        vect_sum = vectorize(['f4(f4, f4)', 'f8(f8, f8)'],
-                             target=target)(sum)
-
-        A = np.random.random(N).astype(np.float32)
-        B = np.random.random(N).astype(np.float32)
-        assert A.shape == B.shape
-        assert A.dtype ==  B.dtype
-        assert len(A.shape) == 1
-
-        D = np.empty(A.shape, dtype=A.dtype)
-
-        print('Data size', N)
-
-        ts = time()
-        D = vect_sum(A, B)
-        te = time()
-
-        total_time = (te - ts)
-
-        print('Execution time %.4f' % total_time)
-        print('Throughput %.4f' % (N / total_time))
-
-        if '-verify' in sys.argv[1:]:
-            check_answer(D, A, B, C)
-
+    start = timer()
+    C = VectorAdd_GPU(A, B)
+    vectoradd_gpu_time = timer() - start
+    
+    print("VectorAdd_CPU took %f seconds" % vectoradd_cpu_time)
+    print("VectorAdd_GPU took %f seconds" % vectoradd_gpu_time) 
 
 if __name__ == '__main__':
     main()
